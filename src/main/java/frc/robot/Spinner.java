5a3c90cb1a68ab7035087d19cfce7cf1f557832b
teamursamajor <teamursamajor@gmail.com>
2020-01-04 19:47:03 -0500

    End of day commit

--- src/main/java/frc/robot/Spinner.java
+++ src/main/java/frc/robot/Spinner.java
@@ -1,5 +1,13 @@
 package frc.robot;
 
+// import edu.wpi.first.wpilibj.Spark;
+
 public class Spinner {
+
+    // private Spark spinSpark;
+
+    // public Spinner() {
+    //     spinSpark = new Spark();
+    // }
 
 }
