842798dc00cb4069f84192f703c4422c4fd41aff
teamursamajor <teamursamajor@gmail.com>
2020-01-27 10:18:55 -0500

    This commit will add NumberTask

---
+++ src/main/java/frc/tasks/NumberTask.java
@@ -0,0 +1,17 @@
+package frc.tasks;
+
+public class NumberTask extends Task {
+
+    private double number;
+
+    public NumberTask(double number) {
+        this.number = number;
+    }
+
+    @Override
+    public void run() {}
+
+    public double getData() {
+        return number;
+    }
+}
