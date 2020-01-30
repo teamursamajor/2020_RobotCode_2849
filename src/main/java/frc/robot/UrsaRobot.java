dda24a6a0d79e06e8ce3526f1f9aac55469a3012
teamursamajor <teamursamajor@gmail.com>
2020-01-30 17:27:47 -0500

    Updating ports

--- src/main/java/frc/robot/UrsaRobot.java
+++ src/main/java/frc/robot/UrsaRobot.java
@@ -22,7 +22,7 @@ public interface UrsaRobot {
 	public static final int DRIVE_FRONT_LEFT = 7, DRIVE_BACK_LEFT = 1;
 	public static final int DRIVE_FRONT_RIGHT = 6, DRIVE_BACK_RIGHT = 8;
 
-	public static final int SPINNER = 9, SHOOTER = 4, BELT = 3;
+	public static final int SPINNER = 3, SHOOTER = 4, BELT = 9;
 	
 	public static final int CLIMB_FRONT = 5, CLIMB_BACK = 0;
 	public static final int INTAKE_MOTOR = 2;
